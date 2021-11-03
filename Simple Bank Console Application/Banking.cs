9cf4649acaeba4261f3175d64c136317d39fa776
Horace <horacewillie7@gmail.com>
2021-11-03 06:28:44 +0100

    Add project files.

---
+++ Simple Bank Console Application/Banking.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Bank_Console_Application
+{
+    class Bank
+    {
+        private double balance = 10000;
+        public double bal
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+    }
+    class  TestBank
+        {
+            Bank Wema = new Bank();
+            string name;
+            int account;
+            double withdraw, deposit, total;
+
+            public void ReceiveMoney()
+            {
+                Console.WriteLine("Enter Name of depositor:");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter Account Number:");
+                account = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Deposit Amount:");
+                deposit = Convert.ToDouble(Console.ReadLine());
+                total = Wema.bal + deposit;
+
+                Console.WriteLine("*********************\n");
+                Console.WriteLine("Name of depositior: " + name);
+                Console.WriteLine("Account Number: " + account);
+                Console.WriteLine("Total  Balance amount in the account: " + total);
+                
+            }
+
+            public void SendMoney()
+            {
+                Console.WriteLine("Emter Account Name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter Account Number: ");
+                account = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Withdraw Amount: ");
+                withdraw = Convert.ToDouble(Console.ReadLine());
+                if(withdraw <= Wema.bal)
+                {
+                    total = Wema.bal - withdraw;
+                    Console.WriteLine("******************");
+                    Console.WriteLine("Account Name: " + name);
+                    Console.WriteLine("Account Number: " + account);
+                    Console.WriteLine("After Withdraw Amount balance is : " + total);
+
+                }
+                else
+                {
+                    Console.WriteLine("Cannot Withdraw as amount is not enough!");
+                }
+            }
+
+            public double CheckBalance()
+            {
+                return Wema.bal;
+            }
+            
+        }
+
+   
+}
---
+++ Simple Bank Console Application/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Simple_Bank_Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char run;
+            do
+            {
+                TestBank Wema = new TestBank();
+                int num;
+                Console.WriteLine("Please Select an Action");
+                Console.WriteLine("\nPress 1 for Deposit an Amount. \nPress 2 to Withdraw an Amount. \nPress 3 to Chcek Balance");
+                num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Wema.ReceiveMoney();
+                        break;
+                    case 2:
+                        Wema.SendMoney();
+                        break;
+                    case 3:
+                        Wema.CheckBalance();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Selection!!!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue this program? (y/n)");
+                run = Convert.ToChar(Console.ReadLine());
+            }
+            while (run == 'y');
+
+            Console.ReadKey();
+        }
+    }
+}
